838b46a6c067c60dbdc2d22ea4a1d62c3fda48c1
mzubairhassan <mzubairhassan18@gmail.com>
2019-10-23 18:30:47 +0500

    pages added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,23 @@ import React from 'react';
 import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom';
 
 import AuthPage from './pages/Auth';
+import MainNavigation from './navigation/MainNavigation';
+
 
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Redirect path="/" to="/auth" exact />
-        <Route path="/auth" component={AuthPage} />
-        <Route path="/events" component={null} />
-        <Route path="/bookings" component={null} />
-      </Switch>
+      <React.Fragment>
+        <MainNavigation />
+        <Switch>
+          <Redirect path="/" to="/auth" exact />
+          <Route path="/auth" component={AuthPage} />
+          <Route path="/events" component={null} />
+          <Route path="/bookings" component={null} />
+        </Switch>
+      </React.Fragment>
     </BrowserRouter>
   );
 }
